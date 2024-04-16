957ea8deb46711fad2c01b14c9035317ce87fc3c
coactor <2211856091@qq.com>
2024-04-16 19:21:15 +0800

    4.16 10:00 reconstruct code

---
+++ org.opencv/build/intermediates/cxx/Debug/2p5q334u/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\coactor\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\coactor\\AndroidStudioProjects\\devtest\\org.opencv\\.cxx\\Debug\\2p5q334u\\arm64-v8a" ^
+  opencv_jni_shared
